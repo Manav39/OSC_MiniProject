f1c93767afc880e54d3c379ed8bfc5adf0177ee2
Aaditya8C <aadityajp0419@gmail.com>
2024-11-19 22:43:11 +0530

    alumini_old_code

--- frontend/src/components/AlumniList.jsx
+++ frontend/src/components/AlumniList.jsx
@@ -1,32 +1,39 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { FaSearch, FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 import defaultavatar from "../assets/uploads/defaultavatar.jpg";
-import alumniData from "./storage/alumini.json"; // Importing the local JSON file
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 const AlumniList = () => {
   const [alumniList, setAlumniList] = useState([]);
-  const [filteredAlumni, setFilteredAlumni] = useState([]);
+  const [filteredAlumni, setFilteredAlumnni] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
 
-  // Use the local JSON data instead of fetching from an API
   useEffect(() => {
-    setAlumniList(alumniData);
-    setFilteredAlumni(alumniData); // Initialize with all alumni
+    axios
+      .get("http://localhost:3000/auth/alumni_list")
+      .then((res) => {
+        console.log(res.data);
+        setAlumniList(res.data);
+      })
+      .catch((err) => console.log(err));
   }, []);
 
   const handleSearchInputChange = (e) => {
     setSearchQuery(e.target.value);
   };
 
-  // Filter alumni based on the search query
   useEffect(() => {
-    const filteredList = alumniList.filter(
-      (alumni) =>
-        alumni.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
-        alumni.course.toLowerCase().includes(searchQuery.toLowerCase()) ||
-        alumni.batch.toString().includes(searchQuery)
-    );
-    setFilteredAlumni(filteredList);
+    if (alumniList.length > 0) {
+      const filteredlist = alumniList.filter(
+        (list) =>
+          list.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+          list.course.toLowerCase().includes(searchQuery.toLowerCase()) ||
+          list.batch.toString().includes(searchQuery)
+      );
+      setFilteredAlumnni(filteredlist);
+    }
   }, [searchQuery, alumniList]);
 
   return (
@@ -41,110 +48,134 @@ const AlumniList = () => {
           </div>
         </div>
       </header>
-      <div className="container mt-4">
-        <div className="card mb-4">
-          <div className="card-body">
-            <div className="row">
-              <div className="col-md-8">
-                <div className="input-group mb-3">
-                  <div className="input-group-prepend">
-                    <span className="input-group-text" id="filter-field">
-                      <FaSearch />
-                    </span>
+      {alumniList.length > 0 && (
+        <div className="container mt-4">
+          <div className="card mb-4">
+            <div className="card-body">
+              <div className="row">
+                <div className="col-md-8">
+                  <div className="input-group mb-3">
+                    <div className="input-group-prepend">
+                      <span className="input-group-text" id="filter-field">
+                        <FaSearch />
+                      </span>
+                    </div>
+                    <input
+                      value={searchQuery}
+                      onChange={handleSearchInputChange}
+                      type="text"
+                      className="form-control"
+                      id="filter"
+                      placeholder="Filter name, course, batch"
+                      aria-label="Filter"
+                      aria-describedby="filter-field"
+                    />
                   </div>
-                  <input
-                    value={searchQuery}
-                    onChange={handleSearchInputChange}
-                    type="text"
-                    className="form-control"
-                    placeholder="Filter name, course, batch"
-                    aria-label="Filter"
-                  />
+                </div>
+                <div className="col-md-4">
+                  <button className="btn btn-primary btn-block" id="search">
+                    Search
+                  </button>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
       <div className="container-fluid mt-3 pt-2">
         {filteredAlumni.length > 0 ? (
-          <div className="row">
-            {filteredAlumni.map((alumni, index) => (
-              <div className="col-md-4 mb-4" key={index}>
-                <div className="card">
-                  <center>
-                    {alumni.avatar ? (
-                      <img
-                        src={alumni.avatar}
-                        alt="avatar"
-                        className="card-img-top img-fluid alimg mt-3"
-                        style={{ objectFit: "cover" }}
-                      />
-                    ) : (
-                      <img
-                        src={defaultavatar}
-                        alt="avatar"
-                        className="card-img-top img-fluid alimg mt-3"
-                      />
-                    )}
-                  </center>
-                  <div className="card-body">
-                    <h5 className="card-title text-center h3">
-                      {alumni.name}{" "}
-                      {alumni.status === 1 && (
+          <>
+            <div className="row">
+              {filteredAlumni.map((a, index) => (
+                <div className="col-md-4 mb-4" key={index}>
+                  <div className="card">
+                    <center>
+                      {a.avatar ? (
+                        <img
+                          src={`http://localhost:3000/${a.avatar}`}
+                          style={{ objectFit: "cover" }}
+                          className="card-img-top img-fluid alimg mt-3"
+                          alt="avatar"
+                        />
+                      ) : (
+                        <>
+                          <img
+                            src={defaultavatar}
+                            className="card-img-top img-fluid alimg mt-3"
+                            alt="avatar"
+                          />
+                        </>
+                      )}
+                    </center>
+                    <div className="card-body">
+                      <h5 className="card-title text-center pad-zero h3">
+                        {a.name}{" "}
                         <small>
-                          <i className="badge badge-primary">Verified</i>
+                          <i
+                            className={`badge badge-primary ${
+                              a.status === 1 ? "" : "d-none"
+                            }`}
+                          >
+                            Verified
+                          </i>
                         </small>
-                      )}
-                    </h5>
-                    <p className="card-text h5">
-                      <strong>Email:</strong> {alumni.email}
-                    </p>
-                    <p className="card-text h5">
-                      <strong>Course:</strong> {alumni.course}
-                    </p>
-                    {alumni.batch && alumni.batch !== "0000" && (
+                      </h5>
+
                       <p className="card-text h5">
-                        <strong>Batch:</strong> {alumni.batch}
+                        <strong>Email:</strong> {a.email}
                       </p>
-                    )}
-                    {alumni.connected_to && (
-                      <p className="card-text h5">
-                        <strong>Currently working in/as:</strong>{" "}
-                        {alumni.connected_to}
-                      </p>
-                    )}
-                    <div className="text-center">
-                      {alumni.github && (
-                        <a
-                          href={alumni.github}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          style={{ marginRight: "15px" }}
-                        >
-                          <FaGithub />
-                        </a>
+                      {a.course && (
+                        <p className="card-text h5">
+                          <strong>Course:</strong> {a.course}
+                        </p>
                       )}
-                      {alumni.linkedin && (
-                        <a
-                          href={alumni.linkedin}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <FaLinkedin />
-                        </a>
+                      {a.batch != "0000" && (
+                        <p className="card-text h5">
+                          <strong>Batch:</strong> {a.batch}
+                        </p>
                       )}
+                      {a.connected_to && (
+                        <p className="card-text h5">
+                          <strong>Currently working in/as:</strong>{" "}
+                          {a.connected_to}
+                        </p>
+                      )}
+
+                      <h5 className="card-title text-center pad-zero h3">
+                        <small>
+                          <a
+                            href={`http://github.com/${a.name}`}
+                            target="_blank"
+                            style={{ color: "inherit" }}
+                          >
+                            <i style={{ marginRight: "30px" }}>
+                              <FaGithub />
+                            </i>
+                          </a>
+                          <a
+                            href={`http://linkedin.com/${a.name}`}
+                            target="_blank"
+                            style={{ color: "inherit" }}
+                          >
+                            <i style={{ marginLeft: "30px" }}>
+                              <FaLinkedin />
+                            </i>
+                          </a>
+                        </small>
+                      </h5>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </div>
+              ))}
+            </div>
+          </>
         ) : (
-          <div className="d-flex flex-column justify-content-center align-items-center">
-            <p>{searchQuery}</p>
-            <h4 className="text-info-emphasis">No Data Available</h4>
-          </div>
+          <>
+            <div className="d-flex flex-column justify-content-center align-items-center">
+              <p>{searchQuery}</p>
+              <h4 className="text-info-emphasis">No Data Available</h4>
+            </div>
+          </>
         )}
       </div>
     </>
